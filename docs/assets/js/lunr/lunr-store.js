024d02e2d3873470ed11d81ed2f6d51a733a4d96
Dave Carroll <powershell@anovelidea.org>
2018-08-14 22:57:41 -0500

    site config

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Welcome to My PowerShell Blog",
         "excerpt":"Greetings fellow PowerShell aficionados! I feel I must begin with a confession: I was not an early adopter of PowerShell. I began using PowerShell because I had no other choice. I needed to manage the Live@EDU environment at the college where I worked. Who had time to learn something that...","categories": ["blog","powershell"],
         "tags": [],
-        "url": "/blog/powershell/blog-inception/",
+        "url": "https://powershell.anovelidea.org/blog/powershell/blog-inception/",
         "teaser":null}]
