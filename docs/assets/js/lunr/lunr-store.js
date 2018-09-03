3da0f5ff16485c291cd15ade7ea2e065cf8f8309
Dave Carroll <powershell@anovelidea.org>
2018-09-03 12:59:11 -0500

    upgrade to minimal-mistakes 4.12.2

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -6,11 +6,21 @@ var store = [{
         "teaser":null},{
         "title": "Unique Email Domains",
         "excerpt":"So you’re working on getting all of your email domains that’s on-prem into Azure or you simply want to ensure that youron-prem Exchange has been configured for all the domains your user accounts utilize. Your Active Directory domaincontains over 30,000 accounts. How would you go about discovering the unique email...","categories": ["powershell"],
-        "tags": ["powershell","exchange"],
+        "tags": ["powershell","exchange","azure"],
         "url": "https://powershell.anovelidea.org/powershell/unique-email-domains/",
         "teaser":null},{
         "title": "Clear DNS Server Cache",
-        "excerpt":"How often have you needed to clear the DNS server cache? It may not be frequent, but if your environment has several domain controllers, a few simple lines of PowerShell canclear the cache on all of them. First, we need to get the name of the domain. Of course, if...","categories": ["powershell"],
+        "excerpt":"How often have you needed to clear the DNS server cache? Regardless how many domain controllers in your environment, a few simple lines of PowerShell can clear the cache on all of them. First, we need to get the name of the domain. Of course, if you already know the...","categories": ["powershell"],
         "tags": ["powershell","dns","dns cache"],
         "url": "https://powershell.anovelidea.org/powershell/clear-dns-server-cache/",
+        "teaser":null},{
+        "title": "Updatable Help",
+        "excerpt":"PowerShell has provided a way to update the help files for modules since version 3.0. Since creating my first two modules, I have tried adding updatable help support, but came across a few issues. Tonight,I finally resolved them for my module PoShEvents. GitHub Bad, GitHub Pages Good My module repository...","categories": ["powershell"],
+        "tags": ["update-help","updatable help","tls","ssl","platyps","powershell"],
+        "url": "https://powershell.anovelidea.org/powershell/updatable-help/",
+        "teaser":null},{
+        "title": "How I Blog",
+        "excerpt":"Someone asked what I used to produce this blog. This post will provide the methodology and details. The Decision to Blog I’ve hosted a personal website on various platforms since early 2000. I’ve used FTP, SFTP, FrontPage, WebDAV,Dreamweaver, WordPress, and probably some others that I’ve forgotten about. Using WordPress, I...","categories": ["blog"],
+        "tags": ["blog","jekyll","travis ci","github","staticman"],
+        "url": "https://powershell.anovelidea.org/blog/how-i-blog/",
         "teaser":null}]
